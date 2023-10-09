b681f8744ec882ff1e588e334772fb6286cdc3a8
Jesse de Wit <witdejesse@hotmail.com>
2023-10-09 10:01:50 +0200

    add 'connect' method
    .
    
    undo delay
    
    also undo delay

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use bip39::{Language, Mnemonic};
+use breez_sdk_core::{
+    BreezEvent, BreezServices, EnvironmentType, EventListener, GreenlightNodeConfig, NodeConfig,
+};
 use clap::{Parser, Subcommand};
 use dotenv::dotenv;
 use log::info;
-use std::env;
+use std::{env, sync::Arc};
 
 #[tokio::main]
 async fn main() {
@@ -57,4 +60,41 @@ fn get_env_var(name: &str) -> Result<String, String> {
     }
 
     Ok(v)
+}
+
+async fn connect() -> Arc<BreezServices> {
+    let breez_sdk_api_key =
+        get_env_var("BREEZ_API_KEY").expect("set the 'BREEZ_API_KEY' environment variable");
+    let greenlight_invite_code = get_env_var("GREENLIGHT_INVITE_CODE")
+        .expect("set the 'GREENLIGHT_INVITE_CODE' environment variable");
+    let phrase = get_env_var("MNEMONIC").expect("set the 'MNEMONIC' environment variable");
+
+    let mnemonic = Mnemonic::parse(phrase).unwrap();
+    let seed = mnemonic.to_seed("");
+
+    let mut config = BreezServices::default_config(
+        EnvironmentType::Production,
+        breez_sdk_api_key,
+        NodeConfig::Greenlight {
+            config: GreenlightNodeConfig {
+                invite_code: Some(greenlight_invite_code),
+                partner_credentials: None,
+            },
+        },
+    );
+    config.exemptfee_msat = 50000;
+
+    let sdk = BreezServices::connect(config, seed.to_vec(), Box::new(AppEventListener {}))
+        .await
+        .unwrap();
+    sdk
+}
+
+struct AppEventListener {}
+impl EventListener for AppEventListener {
+    fn on_event(&self, e: BreezEvent) {
+        match e {
+            _ => return,
+        }
+    }
 }
